dc455b29061fad3226028aafd35f7bf197593eba
Helmi Hnich <hh@knowlepsy.com>
2026-04-16 00:22:29 +0100

    feat: implement visualizer route with image comparison and add file upload component

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -4,6 +4,7 @@ import { generate3DView } from '../../lib/ai.action';
 import { Box, Download, RefreshCcw, Share2, X } from 'lucide-react';
 import { Button } from '../../components/ui/Button';
 import { createProject, getProjectById } from '../../lib/puter.action';
+import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 
 const VisualizerId = () => {
   const navigate = useNavigate()
@@ -12,14 +13,23 @@ const VisualizerId = () => {
   const [project, setProject] = useState<DesignItem | null>(null)
   const [isProjectLoading, setIsProjectLoading] = useState(true)
   
-  const [projectData, setProjectData] = useState<{ initialImage?: string, initialRendered?: string, name?: string }>({});
-
   const hasInitialGenerated = useRef(false)
 
   const [isProcessing, setIsProcessing] = useState(false)
   const [currentImage, setCurrentImage] = useState<string | null>(null)
 
   const handleBack = () => navigate('/')
+
+  const handleExport = () => {
+    if (!currentImage) return;
+
+    const link = document.createElement('a');
+    link.href = currentImage;
+    link.download = `roomify-render-${id}.png`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
 
   const runGeneration = async (item: DesignItem) =>{
@@ -122,7 +132,7 @@ const VisualizerId = () => {
             <div className="panel-actions">
               <Button
                 size='sm'
-                onClick={()=>{}}
+                onClick={handleExport}
                 className='export'
                 disabled={!currentImage}
                 >
@@ -161,6 +171,39 @@ const VisualizerId = () => {
                     <span className='title'>Rendering ...</span>
                     <span className='subtitle'>Generating your 3D visualization ...</span>
                   </div>
+                </div>
+              )
+            }
+          </div>
+        </div>
+        <div className="panel compare">
+          <div className="panel-header">
+            <div className="panel-meta">
+              <p>Comparison</p>
+              <h3>Before and After</h3>
+            </div>
+            <div className="hint">Drag to compare</div>
+          </div>
+          <div className="compare-stage">
+            {
+              project?.sourceImage && currentImage ? (
+                <ReactCompareSlider
+                  defaultValue={50}
+                  style={{width: '100%', height: 'auto'}}
+                  itemOne={
+                    <ReactCompareSliderImage src={project?.sourceImage} alt='before' className='compare-img'/>
+                  }
+                  itemTwo={
+                    <ReactCompareSliderImage src={currentImage} alt='after' className='compare-img'/>
+                  }
+                />
+              ) : (
+                <div className="compare-fallback">
+                  {
+                    project?.sourceImage && (
+                      <img src='project.sourceImage' alt='Before' className='compare-img'/>
+                    )
+                  }
                 </div>
               )
             }
